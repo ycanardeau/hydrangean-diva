7216285779c683886cf2fd79d1671d6e2f4da3e2
Aigamo <ycanardeau@gmail.com>
2023-06-11 02:34:39 +0000

    Create HydrangeanDiva

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,59 +1,14 @@
-import { BottomBar } from '@/components/BottomBar';
-import { MiniPlayer } from '@/components/MiniPlayer';
-import { PlayQueueTable } from '@/components/PlayQueueTable';
+import { HydrangeanDiva } from '@/components/HydrangeanDiva';
 import { usePlayerStore } from '@/components/PlayerStoreContext';
-import { useNostalgicDiva } from '@aigamo/nostalgic-diva';
-import { EuiButton, EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
-import { reaction } from 'mobx';
-import { observer } from 'mobx-react-lite';
 import React from 'react';
 
-export const AppContainer = observer((): React.ReactElement => {
+export const AppContainer = (): React.ReactElement => {
 	const playerStore = usePlayerStore();
-	const diva = useNostalgicDiva();
-
-	React.useEffect(() => {
-		return reaction(
-			() => playerStore.playQueueStore.currentItem,
-			async (currentItem, previousItem) => {
-				if (currentItem === undefined || previousItem === undefined) {
-					return;
-				}
-
-				if (currentItem.id === previousItem.id) {
-					await diva.setCurrentTime(0);
-				}
-			},
-		);
-	}, [playerStore, diva]);
 
 	return (
-		<>
-			<EuiFlexGroup alignItems="center" gutterSize="m">
-				<EuiFlexItem grow={false}>
-					<EuiButton
-						onClick={(): void => playerStore.playQueueStore.clear()}
-					>
-						Clear{/* LOC */}
-					</EuiButton>
-				</EuiFlexItem>
-			</EuiFlexGroup>
-
-			<EuiSpacer size="l" />
-
-			<PlayQueueTable playQueueStore={playerStore.playQueueStore} />
-
-			{!playerStore.playQueueStore.isEmpty && (
-				<MiniPlayer
-					playerStore={playerStore}
-					playQueueStore={playerStore.playQueueStore}
-				/>
-			)}
-
-			<BottomBar
-				playerStore={playerStore}
-				playQueueStore={playerStore.playQueueStore}
-			/>
-		</>
+		<HydrangeanDiva
+			playerStore={playerStore}
+			playQueueStore={playerStore.playQueueStore}
+		/>
 	);
-});
+};
---
+++ src/components/HydrangeanDiva.tsx
@@ -0,0 +1,70 @@
+import { BottomBar } from '@/components/BottomBar';
+import { MiniPlayer } from '@/components/MiniPlayer';
+import { PlayQueueTable } from '@/components/PlayQueueTable';
+import { PlayQueueStore } from '@/stores/PlayQueueStore';
+import { PlayerStore } from '@/stores/PlayerStore';
+import { useNostalgicDiva } from '@aigamo/nostalgic-diva';
+import { EuiButton, EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
+import { reaction } from 'mobx';
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+
+interface HydrangeanDivaProps {
+	playerStore: PlayerStore;
+	playQueueStore: PlayQueueStore;
+}
+
+export const HydrangeanDiva = observer(
+	({
+		playerStore,
+		playQueueStore,
+	}: HydrangeanDivaProps): React.ReactElement => {
+		const diva = useNostalgicDiva();
+
+		React.useEffect(() => {
+			return reaction(
+				() => playQueueStore.currentItem,
+				async (currentItem, previousItem) => {
+					if (
+						currentItem === undefined ||
+						previousItem === undefined
+					) {
+						return;
+					}
+
+					if (currentItem.id === previousItem.id) {
+						await diva.setCurrentTime(0);
+					}
+				},
+			);
+		}, [playQueueStore, diva]);
+
+		return (
+			<>
+				<EuiFlexGroup alignItems="center" gutterSize="m">
+					<EuiFlexItem grow={false}>
+						<EuiButton onClick={(): void => playQueueStore.clear()}>
+							Clear{/* LOC */}
+						</EuiButton>
+					</EuiFlexItem>
+				</EuiFlexGroup>
+
+				<EuiSpacer size="l" />
+
+				<PlayQueueTable playQueueStore={playQueueStore} />
+
+				{!playQueueStore.isEmpty && (
+					<MiniPlayer
+						playerStore={playerStore}
+						playQueueStore={playQueueStore}
+					/>
+				)}
+
+				<BottomBar
+					playerStore={playerStore}
+					playQueueStore={playQueueStore}
+				/>
+			</>
+		);
+	},
+);
