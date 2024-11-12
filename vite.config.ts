afc10650dc2d26c23cf7a226a69a42c64d0be705
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2024-11-12 21:08:46 +0900

    Configure vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,36 @@ export default defineConfig({
 		react({
 			// https://dev.to/ajitsinghkamal/using-emotionjs-with-vite-2ndj#comment-1nif3
 			jsxImportSource: '@emotion/react',
+		}),
+		VitePWA({
+			strategies: 'injectManifest',
+			srcDir: 'src',
+			filename: 'sw.ts',
+			registerType: 'autoUpdate',
+			injectRegister: false,
+
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
+
+			manifest: {
+				name: 'Hydrangean Diva',
+				short_name: 'Hydrangean Diva',
+				description: '',
+				theme_color: '#ffffff',
+			},
+
+			injectManifest: {
+				globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+			},
+
+			devOptions: {
+				enabled: false,
+				navigateFallback: 'index.html',
+				suppressWarnings: true,
+				type: 'module',
+			},
 		}),
 	],
 	build: {
