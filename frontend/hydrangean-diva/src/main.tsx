b8297928079bd69842f07eea61794d1dadc98d5a
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2024-11-21 01:01:49 +0900

    Upgrade to React 18

--- frontend/hydrangean-diva/src/main.tsx
+++ frontend/hydrangean-diva/src/main.tsx
@@ -1,10 +1,9 @@
 import App from '@/App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
-	document.getElementById('root') as HTMLElement,
 );
