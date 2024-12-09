ed3527233b1d37cbbd3cd97946458910dcf91fba
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2024-12-10 07:44:29 +0900

    Create AddVideoButton

---
+++ frontend/hydrangean-diva/src/components/AddVideoButton.tsx
@@ -0,0 +1,158 @@
+import { PlayQueueStore } from '@/stores/PlayQueueStore';
+import { findVideoService } from '@aigamo/nostalgic-diva';
+import {
+	EuiButton,
+	EuiButtonEmpty,
+	EuiFieldText,
+	EuiForm,
+	EuiFormRow,
+	EuiModal,
+	EuiModalBody,
+	EuiModalFooter,
+	EuiModalHeader,
+	EuiModalHeaderTitle,
+} from '@elastic/eui';
+import { AddRegular } from '@fluentui/react-icons';
+import React from 'react';
+
+interface AddVideoModalProps {
+	onCancel: () => void;
+	onSave: (e: { url: string; title: string }) => Promise<void>;
+}
+
+const AddVideoModal = ({
+	onCancel,
+	onSave,
+}: AddVideoModalProps): React.ReactElement => {
+	const [url, setUrl] = React.useState('');
+	const [title, setTitle] = React.useState('');
+	const [loading, setLoading] = React.useState(false);
+
+	return (
+		<EuiModal onClose={onCancel} initialFocus="[name=url]">
+			<EuiModalHeader>
+				<EuiModalHeaderTitle>Add video{/* LOC */}</EuiModalHeaderTitle>
+			</EuiModalHeader>
+
+			<EuiModalBody>
+				<EuiForm component="form">
+					<EuiFormRow label="URL" /* LOC */>
+						<EuiFieldText
+							name="url"
+							value={url}
+							onChange={(e): void => setUrl(e.target.value)}
+						/>
+					</EuiFormRow>
+
+					<EuiFormRow label="Title">
+						<EuiFieldText
+							name="title"
+							value={title}
+							onChange={(e): void => setTitle(e.target.value)}
+						/>
+					</EuiFormRow>
+				</EuiForm>
+			</EuiModalBody>
+
+			<EuiModalFooter>
+				<EuiButtonEmpty onClick={onCancel}>
+					Cancel{/* LOC */}
+				</EuiButtonEmpty>
+
+				<EuiButton
+					type="submit"
+					onClick={async (): Promise<void> => {
+						try {
+							setLoading(true);
+
+							await onSave({ url, title });
+						} finally {
+							setLoading(false);
+						}
+					}}
+					fill
+					disabled={url.trim().length === 0}
+					isLoading={loading}
+				>
+					Save{/* LOC */}
+				</EuiButton>
+			</EuiModalFooter>
+		</EuiModal>
+	);
+};
+
+interface NoembedResult {
+	title: string;
+}
+
+function isNoembedResult(value: any): value is NoembedResult {
+	return (
+		value !== null &&
+		typeof value === 'object' &&
+		'title' in value &&
+		typeof value.title === 'string'
+	);
+}
+
+interface AddVideoButtonProps {
+	playQueueStore: PlayQueueStore;
+}
+
+export const AddVideoButton = React.memo(
+	({ playQueueStore }: AddVideoButtonProps): React.ReactElement => {
+		const [addVideoModalOpen, setAddVideoModalOpen] = React.useState(false);
+
+		const handleSave = React.useCallback(
+			async (e: { url: string; title: string }): Promise<void> => {
+				const videoService = findVideoService(e.url);
+				if (videoService !== undefined) {
+					const videoId = videoService.extractVideoId(e.url);
+					if (videoId !== undefined) {
+						const response = await fetch(
+							`https://noembed.com/embed?url=${encodeURIComponent(
+								e.url,
+							)}`,
+						);
+						const jsonData = await response.json();
+
+						await playQueueStore.addItems([
+							playQueueStore.createItem({
+								url: e.url,
+								type: videoService.type,
+								videoId: videoId,
+								title:
+									e.title ||
+									(isNoembedResult(jsonData)
+										? jsonData.title
+										: videoId),
+							}),
+						]);
+					}
+				}
+
+				setAddVideoModalOpen(false);
+			},
+			[playQueueStore],
+		);
+
+		return (
+			<>
+				<EuiButton
+					onClick={(): void => setAddVideoModalOpen(true)}
+					iconType={AddRegular}
+					size="s"
+					color="primary"
+				>
+					Add video{/* LOC */}
+				</EuiButton>
+
+				{addVideoModalOpen && (
+					<AddVideoModal
+						onCancel={(): void => setAddVideoModalOpen(false)}
+						onSave={handleSave}
+					/>
+				)}
+			</>
+		);
+	},
+);
--- frontend/hydrangean-diva/src/components/Header.tsx
+++ frontend/hydrangean-diva/src/components/Header.tsx
@@ -1,8 +1,7 @@
+import { AddVideoButton } from '@/components/AddVideoButton';
 import { PlayQueueStore } from '@/stores/PlayQueueStore';
-import { findVideoService } from '@aigamo/nostalgic-diva';
 import {
 	EuiBadge,
-	EuiButton,
 	EuiCollapsibleNav,
 	EuiCollapsibleNavGroup,
 	EuiFlexItem,
@@ -15,86 +14,7 @@ import {
 	EuiToolTip,
 	useGeneratedHtmlId,
 } from '@elastic/eui';
-import { AddRegular } from '@fluentui/react-icons';
 import React from 'react';
-
-import { AddVideoModal } from './AddVideoModal';
-
-interface NoembedResult {
-	title: string;
-}
-
-function isNoembedResult(value: any): value is NoembedResult {
-	return (
-		value !== null &&
-		typeof value === 'object' &&
-		'title' in value &&
-		typeof value.title === 'string'
-	);
-}
-
-interface AddVideoButtonProps {
-	playQueueStore: PlayQueueStore;
-}
-
-export const AddVideoButton = React.memo(
-	({ playQueueStore }: AddVideoButtonProps): React.ReactElement => {
-		const [addVideoModalOpen, setAddVideoModalOpen] = React.useState(false);
-
-		const handleSave = React.useCallback(
-			async (e: { url: string; title: string }): Promise<void> => {
-				const videoService = findVideoService(e.url);
-				if (videoService !== undefined) {
-					const videoId = videoService.extractVideoId(e.url);
-					if (videoId !== undefined) {
-						const response = await fetch(
-							`https://noembed.com/embed?url=${encodeURIComponent(
-								e.url,
-							)}`,
-						);
-						const jsonData = await response.json();
-
-						await playQueueStore.addItems([
-							playQueueStore.createItem({
-								url: e.url,
-								type: videoService.type,
-								videoId: videoId,
-								title:
-									e.title ||
-									(isNoembedResult(jsonData)
-										? jsonData.title
-										: videoId),
-							}),
-						]);
-					}
-				}
-
-				setAddVideoModalOpen(false);
-			},
-			[playQueueStore],
-		);
-
-		return (
-			<>
-				<EuiButton
-					onClick={(): void => setAddVideoModalOpen(true)}
-					iconType={AddRegular}
-					size="s"
-					color="primary"
-				>
-					Add video{/* LOC */}
-				</EuiButton>
-
-				{addVideoModalOpen && (
-					<AddVideoModal
-						onCancel={(): void => setAddVideoModalOpen(false)}
-						onSave={handleSave}
-					/>
-				)}
-			</>
-		);
-	},
-);
 
 interface HeaderProps {
 	playQueueStore: PlayQueueStore;
