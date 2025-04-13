9d5a468b65bdca8039a1de08f358a202b5ffb727
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-04-13 23:18:57 +0900

    Create AddVideoModal

--- frontend/hydrangean-diva/src/components/AddVideoButton.tsx
+++ frontend/hydrangean-diva/src/components/AddVideoButton.tsx
@@ -1,85 +1,9 @@
+import { AddVideoModal } from '@/components/AddVideoModal';
 import { PlayQueueStore } from '@/stores/PlayQueueStore';
 import { findVideoService } from '@aigamo/nostalgic-diva';
-import {
-	EuiButton,
-	EuiButtonEmpty,
-	EuiFieldText,
-	EuiForm,
-	EuiFormRow,
-	EuiModal,
-	EuiModalBody,
-	EuiModalFooter,
-	EuiModalHeader,
-	EuiModalHeaderTitle,
-} from '@elastic/eui';
+import { EuiButton } from '@elastic/eui';
 import { AddRegular } from '@fluentui/react-icons';
 import React from 'react';
-
-interface AddVideoModalProps {
-	onCancel: () => void;
-	onSave: (e: { url: string; title: string }) => Promise<void>;
-}
-
-const AddVideoModal = ({
-	onCancel,
-	onSave,
-}: AddVideoModalProps): React.ReactElement => {
-	const [url, setUrl] = React.useState('');
-	const [title, setTitle] = React.useState('');
-	const [loading, setLoading] = React.useState(false);
-
-	return (
-		<EuiModal onClose={onCancel} initialFocus="[name=url]">
-			<EuiModalHeader>
-				<EuiModalHeaderTitle>Add video{/* LOC */}</EuiModalHeaderTitle>
-			</EuiModalHeader>
-
-			<EuiModalBody>
-				<EuiForm component="form">
-					<EuiFormRow label="URL" /* LOC */>
-						<EuiFieldText
-							name="url"
-							value={url}
-							onChange={(e): void => setUrl(e.target.value)}
-						/>
-					</EuiFormRow>
-
-					<EuiFormRow label="Title">
-						<EuiFieldText
-							name="title"
-							value={title}
-							onChange={(e): void => setTitle(e.target.value)}
-						/>
-					</EuiFormRow>
-				</EuiForm>
-			</EuiModalBody>
-
-			<EuiModalFooter>
-				<EuiButtonEmpty onClick={onCancel}>
-					Cancel{/* LOC */}
-				</EuiButtonEmpty>
-
-				<EuiButton
-					type="submit"
-					onClick={async (): Promise<void> => {
-						try {
-							setLoading(true);
-
-							await onSave({ url, title });
-						} finally {
-							setLoading(false);
-						}
-					}}
-					fill
-					disabled={url.trim().length === 0}
-					isLoading={loading}
-				>
-					Save{/* LOC */}
-				</EuiButton>
-			</EuiModalFooter>
-		</EuiModal>
-	);
-};
 
 interface NoembedResult {
 	title: string;
