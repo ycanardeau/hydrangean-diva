edb5466530e1ff468d1569ce1b220010ba3776d3
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-04-19 18:36:36 +0900

    Revert "Remove title and text from params"
    This reverts commit cea30553b5feb11947e54f7679bb0425d3667401.

--- frontend/hydrangean-diva/vite.config.ts
+++ frontend/hydrangean-diva/vite.config.ts
@@ -39,6 +39,8 @@ export default defineConfig({
 					method: 'GET',
 					enctype: 'application/x-www-form-urlencoded',
 					params: {
+						title: 'title',
+						text: 'text',
 						url: 'url',
 					},
 				},
