59922a994e5e82839d0bbee7589b297a1ec8ac77
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-11-04 09:00:56 +0900

    Replace React Router with TanStack Router

---
+++ frontend/hydrangean-diva/src/routes/__root.tsx
@@ -0,0 +1,16 @@
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { ReactElement } from 'react';
+
+const RootComponent = (): ReactElement => {
+	return (
+		<>
+			<Outlet />
+			<TanStackRouterDevtools />
+		</>
+	);
+};
+
+export const Route = createRootRoute({
+	component: RootComponent,
+});
