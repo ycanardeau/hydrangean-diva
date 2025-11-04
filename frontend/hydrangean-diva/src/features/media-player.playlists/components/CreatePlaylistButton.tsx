1becbbc2af5d29844bc22aa194d124cda097c723
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-11-04 12:17:47 +0900

    Create CreatePlaylistButton

---
+++ frontend/hydrangean-diva/src/features/media-player.playlists/components/CreatePlaylistButton.tsx
@@ -0,0 +1,95 @@
+import {
+	EuiButton,
+	EuiButtonEmpty,
+	EuiFieldText,
+	EuiForm,
+	EuiFormRow,
+	EuiModal,
+	EuiModalBody,
+	EuiModalFooter,
+	EuiModalHeader,
+	EuiModalHeaderTitle,
+} from '@elastic/eui';
+import { ReactElement, useCallback, useState } from 'react';
+
+interface CreatePlaylistModalProps {
+	onCancel: () => void;
+	onSave: (e: { name: string }) => Promise<void>;
+}
+
+const CreatePlaylistModal = ({
+	onCancel,
+	onSave,
+}: CreatePlaylistModalProps): ReactElement => {
+	const [name, setName] = useState('');
+	const [loading, setLoading] = useState(false);
+
+	return (
+		<EuiModal onClose={onCancel} initialFocus="[name=name]">
+			<EuiModalHeader>
+				<EuiModalHeaderTitle>
+					Create playlist{/* LOC */}
+				</EuiModalHeaderTitle>
+			</EuiModalHeader>
+
+			<EuiModalBody>
+				<EuiForm component="form">
+					<EuiFormRow label="Name">
+						<EuiFieldText
+							name="name"
+							value={name}
+							onChange={(e): void => setName(e.target.value)}
+						/>
+					</EuiFormRow>
+				</EuiForm>
+			</EuiModalBody>
+
+			<EuiModalFooter>
+				<EuiButtonEmpty onClick={onCancel}>
+					Cancel{/* LOC */}
+				</EuiButtonEmpty>
+
+				<EuiButton
+					type="submit"
+					onClick={async (): Promise<void> => {
+						try {
+							setLoading(true);
+
+							await onSave({ name });
+						} finally {
+							setLoading(false);
+						}
+					}}
+					fill
+					disabled={name.trim().length === 0}
+					isLoading={loading}
+				>
+					Save{/* LOC */}
+				</EuiButton>
+			</EuiModalFooter>
+		</EuiModal>
+	);
+};
+
+export const CreatePlaylistButton = (): ReactElement => {
+	const [isModalOpen, setModalOpen] = useState(false);
+
+	const handleSave = useCallback(async (): Promise<void> => {
+		setModalOpen(false);
+	}, []);
+
+	return (
+		<>
+			<EuiButton onClick={(): void => setModalOpen(true)}>
+				Create playlist{/* LOC */}
+			</EuiButton>
+
+			{isModalOpen && (
+				<CreatePlaylistModal
+					onCancel={(): void => setModalOpen(false)}
+					onSave={handleSave}
+				/>
+			)}
+		</>
+	);
+};
--- frontend/hydrangean-diva/src/features/media-player.playlists/components/PlaylistsEmptyPrompt.tsx
+++ frontend/hydrangean-diva/src/features/media-player.playlists/components/PlaylistsEmptyPrompt.tsx
@@ -1,13 +1,15 @@
-import { EuiButton, EuiEmptyPrompt } from '@elastic/eui';
+import { EuiEmptyPrompt } from '@elastic/eui';
 import { Cd16Regular } from '@fluentui/react-icons';
 import { ReactElement } from 'react';
+
+import { CreatePlaylistButton } from '@/features/media-player.playlists/components/CreatePlaylistButton';
 
 export const PlaylistsEmptyPrompt = (): ReactElement => {
 	return (
 		<EuiEmptyPrompt
 			iconType={Cd16Regular}
 			title={<h2>You don't have any playlists{/* LOC */}</h2>}
-			actions={<EuiButton>Create playlist{/* LOC */}</EuiButton>}
+			actions={<CreatePlaylistButton />}
 		/>
 	);
 };
