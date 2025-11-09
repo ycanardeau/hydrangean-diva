067f83c2941c7330b8c509913d7c15d8874f0596
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-11-09 23:42:53 +0900

    Replace Microsoft.AspNetCore.OpenApi with Swashbuckle.AspNetCore

--- backend/HydrangeanDiva/Program.cs
+++ backend/HydrangeanDiva/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 
 // https://learn.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-8.0#attribute-on-an-assembly
@@ -8,9 +10,55 @@ builder.AddServiceDefaults();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers()
+	.AddJsonOptions(x => x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+	// https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/1607#issuecomment-607170559
+	options.CustomSchemaIds(type => type.ToString());
+
+	// https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/2343#issuecomment-1061233608
+	options.DescribeAllParametersInCamelCase();
+
+	// https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/2036
+	options.SupportNonNullableReferenceTypes();
+	options.NonNullableReferenceTypesAsRequired();
+
+	// https://github.com/kimbell/Kiota.Testing/blob/c52a2bf282eae50da36b221f263cd239f8f9dbf7/Bob.Kiota.Web/Program.cs#L15
+	options.UseAllOfForInheritance();
+	options.UseOneOfForPolymorphism();
+	options.SelectDiscriminatorNameUsing(type =>
+	{
+		// see if the code has been annotated
+		var attribute = type
+			.GetCustomAttributes(true)
+			.OfType<JsonPolymorphicAttribute>()
+			.FirstOrDefault();
+
+		// if nothing has been found, use default name. This is the same for both STJ and Newtonsoft
+		return attribute?.TypeDiscriminatorPropertyName ?? "$type";
+	});
+	options.SelectDiscriminatorValueUsing(type =>
+	{
+		// figures out the value of the discriminator
+		if (type.BaseType is null)
+		{
+			return null;
+		}
+
+		var attribute = type
+			.BaseType
+			.GetCustomAttributes(true)
+			.OfType<JsonDerivedTypeAttribute>()
+			.FirstOrDefault(x => x.DerivedType == type);
+
+		return attribute?.TypeDiscriminator?.ToString();
+	});
+});
 
 HydrangeanDiva.MediaPlayer.Module.ServiceExtensions.AddModule(builder);
 
@@ -21,7 +69,8 @@ app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.MapOpenApi();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
