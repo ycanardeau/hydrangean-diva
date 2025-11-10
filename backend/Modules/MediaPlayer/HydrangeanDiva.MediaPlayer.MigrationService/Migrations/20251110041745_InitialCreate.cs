be0ec65a92613b0d713b93adeaacc1c6022630fe
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-11-10 13:17:52 +0900

    Create migration

---
+++ backend/Modules/MediaPlayer/HydrangeanDiva.MediaPlayer.MigrationService/Migrations/20251110041745_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HydrangeanDiva.MediaPlayer.MigrationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "MediaPlayer");
+
+            migrationBuilder.CreateTable(
+                name: "Playlists",
+                schema: "MediaPlayer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playlists", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Playlists",
+                schema: "MediaPlayer");
+        }
+    }
+}
