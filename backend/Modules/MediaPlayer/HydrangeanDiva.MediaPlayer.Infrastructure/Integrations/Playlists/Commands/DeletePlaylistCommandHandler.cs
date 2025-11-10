3e2e02eaa78369c13848938dbab9d98bfdc52265
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-11-10 15:50:04 +0900

    Implement DeletePlaylistCommandHandler

--- backend/Modules/MediaPlayer/HydrangeanDiva.MediaPlayer.Infrastructure/Integrations/Playlists/Commands/DeletePlaylistCommandHandler.cs
+++ backend/Modules/MediaPlayer/HydrangeanDiva.MediaPlayer.Infrastructure/Integrations/Playlists/Commands/DeletePlaylistCommandHandler.cs
@@ -1,12 +1,28 @@
 using HydrangeanDiva.MediaPlayer.Contracts.Playlists.Commands;
 using HydrangeanDiva.MediaPlayer.Contracts.Playlists.Dtos;
+using HydrangeanDiva.MediaPlayer.Domain.Playlists.Entities;
+using HydrangeanDiva.MediaPlayer.Domain.Playlists.ValueObjects;
+using HydrangeanDiva.MediaPlayer.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace HydrangeanDiva.MediaPlayer.Infrastructure.Integrations.Playlists.Commands;
 
-internal class DeletePlaylistCommandHandler : IRequestHandler<DeletePlaylistCommand, Result<DeletePlaylistResponseDto>>
+internal class DeletePlaylistCommandHandler(ApplicationDbContext dbContext) : IRequestHandler<DeletePlaylistCommand, Result<DeletePlaylistResponseDto>>
 {
+	private async Task<Result<Playlist>> GetPlaylistAsync(PlaylistId id, CancellationToken cancellationToken)
+	{
+		var playlist = await dbContext.Playlists.SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+		return playlist is null
+			? NotFound<Playlist>()
+			: playlist;
+	}
+
 	public Task<Result<DeletePlaylistResponseDto>> Handle(DeletePlaylistCommand request, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
+		return GetPlaylistAsync(new PlaylistId(request.Id), cancellationToken)
+			.Tap(x => dbContext.Playlists.Remove(x))
+			.Tap(x => dbContext.SaveChangesAsync(cancellationToken))
+			.Map(x => new DeletePlaylistResponseDto());
 	}
 }
