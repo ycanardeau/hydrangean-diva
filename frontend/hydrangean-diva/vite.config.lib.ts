7bbc3201cd03e57d0864f0d0eda22da9b9fe49f7
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-12-31 17:14:44 +0900

    Replace assert with with

--- frontend/hydrangean-diva/vite.config.lib.ts
+++ frontend/hydrangean-diva/vite.config.lib.ts
@@ -1,8 +1,9 @@
-import pkg from './package.json' assert { type: 'json' };
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+
+import pkg from './package.json' with { type: 'json' };
 
 // https://vitejs.dev/config/
 export default defineConfig({
--- frontend/hydrangean-diva/vite.config.ts
+++ frontend/hydrangean-diva/vite.config.ts
@@ -1,8 +1,8 @@
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
