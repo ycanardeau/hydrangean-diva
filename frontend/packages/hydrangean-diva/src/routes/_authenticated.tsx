7b25bb3af5f3d0734f18e8eddcb2d789f20762fe
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2026-02-10 00:59:52 +0900

    Move useLocalStorageState from _authenticated to PlayQueueProvider

--- frontend/packages/hydrangean-diva/src/routes/_authenticated.tsx
+++ frontend/packages/hydrangean-diva/src/routes/_authenticated.tsx
@@ -1,21 +1,14 @@
-import { localStorageStateKeys } from '@/features/common/stores/localStorageStateKeys';
 import { Header } from '@/features/media-player.header/components/Header';
 import { usePlayQueue } from '@/features/media-player.play-queue.abstractions/contexts/PlayQueueContext';
 import { MediaPlayerLayout } from '@/features/media-player.player/components/MediaPlayerLayout';
 import { BottomBar } from '@/features/media-player.player/components/bottom-bar/BottomBar';
 import { MiniPlayer } from '@/features/media-player.player/components/mini-player/MiniPlayer';
-import { useLocalStorageState } from '@aigamo/route-sphere';
 import { Outlet, createFileRoute } from '@tanstack/react-router';
 import { observer } from 'mobx-react-lite';
 import type { ReactElement } from 'react';
 
 const RouteComponent = observer((): ReactElement => {
 	const playQueue = usePlayQueue();
-
-	useLocalStorageState(
-		localStorageStateKeys.playQueue,
-		playQueue.localStorageState,
-	);
 
 	return (
 		<>
