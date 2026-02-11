598c5cb203203ebfc57179b26d82d2d8668c6ceb
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2026-02-11 13:37:16 +0900

    Create PlayQueueCommandBar

--- frontend/packages/hydrangean-diva/src/features/media-player.play-queue/components/PlayQueueSection.tsx
+++ frontend/packages/hydrangean-diva/src/features/media-player.play-queue/components/PlayQueueSection.tsx
@@ -1,45 +1,16 @@
-import { featureFlags } from '@/features/common/helpers/featureFlags';
 import type { IPlayQueueStore } from '@/features/media-player.play-queue.abstractions/interfaces/IPlayQueueStore';
 import type { PlayQueueItemDto } from '@/features/media-player.play-queue.abstractions/interfaces/PlayQueueItemDto';
-import { AddToSelectablePopover } from '@/features/media-player.play-queue/components/AddToSelectablePopover';
 import { AddVideoButton } from '@/features/media-player.play-queue/components/AddVideoButton';
-import { DeveloperToolsButton } from '@/features/media-player.play-queue/components/DeveloperToolsButton';
+import { PlayQueueCommandBar } from '@/features/media-player.play-queue/components/PlayQueueCommandBar';
 import { PlayQueueTable } from '@/features/media-player.play-queue/components/PlayQueueTable';
-import { PlayQueueStore } from '@/features/media-player.play-queue/stores/PlayQueueStore';
 import {
-	EuiButton,
 	EuiEmptyPrompt,
-	EuiFlexGroup,
-	EuiFlexItem,
 	EuiPageTemplate,
 	EuiSpacer,
 	useEuiTheme,
 } from '@elastic/eui';
-import {
-	AddRegular,
-	DeleteRegular,
-	DismissRegular,
-} from '@fluentui/react-icons';
 import { observer } from 'mobx-react-lite';
 import { type ReactElement, useCallback } from 'react';
-
-interface AddToPlayQueueButtonProps {
-	playQueue: IPlayQueueStore;
-}
-
-const AddToPlayQueueButton = observer(
-	({ playQueue }: AddToPlayQueueButtonProps): ReactElement => {
-		return (
-			<EuiButton
-				iconType={AddRegular}
-				onClick={playQueue.addSelectedItems}
-				disabled={!playQueue.canAddSelectedItems}
-			>
-				Add to play queue{/* LOC */}
-			</EuiButton>
-		);
-	},
-);
 
 interface PlayQueueSectionProps {
 	playQueue: IPlayQueueStore;
@@ -48,9 +19,6 @@ interface PlayQueueSectionProps {
 export const PlayQueueSection = observer(
 	({ playQueue }: PlayQueueSectionProps): ReactElement => {
 		const { euiTheme } = useEuiTheme();
-
-		const handleAddToPlaylist =
-			useCallback(async (): Promise<void> => {}, []);
 
 		const handleAddVideo = useCallback(
 			async (e: PlayQueueItemDto): Promise<void> => {
@@ -63,62 +31,7 @@ export const PlayQueueSection = observer(
 
 		return (
 			<EuiPageTemplate.Section>
-				<EuiFlexGroup
-					alignItems="center"
-					gutterSize="m"
-					style={{
-						position: 'sticky',
-						top: 48,
-						zIndex: 998,
-						background: euiTheme.colors.backgroundBasePlain,
-					}}
-				>
-					<EuiFlexItem grow={false}>
-						<EuiButton
-							disabled={!playQueue.canPlaySelectedItemsNext}
-							onClick={playQueue.playSelectedItemsNext}
-						>
-							Play next{/* LOC */}
-						</EuiButton>
-					</EuiFlexItem>
-					<EuiFlexItem grow={false}>
-						<AddToPlayQueueButton playQueue={playQueue} />
-					</EuiFlexItem>
-					<EuiFlexItem grow={false}>
-						<AddToSelectablePopover
-							disabled={!featureFlags.mediaPlayer.enablePlaylists}
-							onAddToPlaylist={handleAddToPlaylist}
-						/>
-					</EuiFlexItem>
-					<EuiFlexItem grow={false}>
-						<EuiButton
-							iconType={DismissRegular}
-							onClick={playQueue.removeSelectedItems}
-							disabled={!playQueue.canRemoveSelectedItems}
-						>
-							Remove{/* LOC */}
-						</EuiButton>
-					</EuiFlexItem>
-					<EuiFlexItem grow={false}>
-						<EuiButton
-							iconType={DeleteRegular}
-							onClick={playQueue.clear}
-							disabled={!playQueue.canClear}
-						>
-							Clear{/* LOC */}
-						</EuiButton>
-					</EuiFlexItem>
-					<EuiFlexItem grow={true} />
-					<EuiFlexItem grow={false}>
-						{false && (
-							<DeveloperToolsButton
-								playQueue={playQueue as PlayQueueStore}
-							/>
-						)}
-
-						<AddVideoButton onSave={handleAddVideo} />
-					</EuiFlexItem>
-				</EuiFlexGroup>
+				<PlayQueueCommandBar playQueue={playQueue} />
 
 				<EuiSpacer
 					size="l"
