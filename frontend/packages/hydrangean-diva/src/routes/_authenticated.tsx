5c0cf31576a126be20d0e6e34f618985be84b4bc
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2026-02-15 17:01:58 +0900

    Add play queue button to bottom bar

--- frontend/packages/hydrangean-diva/src/routes/_authenticated.tsx
+++ frontend/packages/hydrangean-diva/src/routes/_authenticated.tsx
@@ -3,12 +3,17 @@ import { Header } from '@/features/media-player.header/components/Header';
 import { MiniPlayer } from '@/features/media-player.mini-player/components/MiniPlayer';
 import { usePlayQueue } from '@/features/media-player.play-queue.abstractions/contexts/PlayQueueContext';
 import { MediaPlayerLayout } from '@/features/media-player/components/MediaPlayerLayout';
-import { Outlet, createFileRoute } from '@tanstack/react-router';
+import { Outlet, createFileRoute, useNavigate } from '@tanstack/react-router';
 import { observer } from 'mobx-react-lite';
 import type { ReactElement } from 'react';
 
 const RouteComponent = observer((): ReactElement => {
 	const playQueue = usePlayQueue();
+
+	const navigate = useNavigate();
+
+	const handleClickPlayQueueButton = (): Promise<void> =>
+		navigate({ to: '/play-queue' });
 
 	return (
 		<>
@@ -20,7 +25,7 @@ const RouteComponent = observer((): ReactElement => {
 
 			{!playQueue.isEmpty && <MiniPlayer />}
 
-			<BottomBar />
+			<BottomBar onClickPlayQueueButton={handleClickPlayQueueButton} />
 		</>
 	);
 });
