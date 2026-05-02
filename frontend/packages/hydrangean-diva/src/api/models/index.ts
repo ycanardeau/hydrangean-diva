1e63ccae664bf61d2061ce3e650d76d03eef9a90
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2026-05-02 23:59:53 +0900

    Remove API clients

--- frontend/packages/hydrangean-diva/src/api/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './HydrangeanDivaMediaPlayerContractsPlaylistsDtosGetPlaylistResponseDto';
-export * from './HydrangeanDivaMediaPlayerContractsPlaylistsDtosListPlaylistsResponseDto';
-export * from './HydrangeanDivaMediaPlayerContractsPlaylistsDtosListTracksResponseDto';
-export * from './HydrangeanDivaMediaPlayerContractsPlaylistsDtosPlaylistDto';
-export * from './HydrangeanDivaMediaPlayerContractsPlaylistsDtosTrackDto';
-export * from './HydrangeanDivaMediaPlayerEndpointsPlaylistsCreatePlaylistRequest';
-export * from './HydrangeanDivaMediaPlayerEndpointsPlaylistsRenamePlaylistRequest';
